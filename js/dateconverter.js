32ce1f2d19f3483f4fbd194cf831acdd11aecf3f
eyob2one <eyob2one@gmail.com>
2024-03-05 04:29:00 -0800

    typo fix in importing

--- js/dateconverter.js
+++ js/dateconverter.js
@@ -1,5 +1,5 @@
 // Assuming all necessary functions from ./main.js are imported correctly
-import { ethTime, toEthiopianDateTime, toEthiopianDateTimeString, toEuropeanDate, toEuropeanDateString } from './main';
+import { ethTime, toEthiopianDateTime, toEthiopianDateTimeString, toEuropeanDate, toEuropeanDateString } from './main.js';
 
 function updateCalculatedEthDateOnPage() {
   const europeanDateValueArray = document.getElementById('EuropeanDate').value.split('-');
